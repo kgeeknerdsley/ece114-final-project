f2d8ba94469854e95c484da0d271d60eeaaa761e
Gavin Worsley <kgeeknerdsley@users.noreply.github.com>
2017-03-01 19:15:17 -0800

    Create point header file

---
+++ Point.h
@@ -0,0 +1,24 @@
+#ifndef __POINT_H_DEFINED__
+#define __POINT_H_DEFINED__
+
+class Point {
+	public:
+		Point();
+		Point(double, double);
+
+		setXPt(double);
+		setYPt(double);
+
+
+		getXPt(double);
+		getYPt(double);
+
+		setXPt(double);
+		setYPt(double);
+
+	private:
+		double xPt;
+		double yPt;
+}
+
+#endif
