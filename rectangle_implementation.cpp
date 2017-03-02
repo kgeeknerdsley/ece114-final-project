1790ccc30f776450b032076266784742a6e53cbe
Gavin Worsley <kgeeknerdsley@users.noreply.github.com>
2017-03-01 21:03:17 -0800

    Added rectangle tests, distance

--- rectangle_implementation.cpp
+++ rectangle_implementation.cpp
@@ -14,21 +14,33 @@ Rectangle::Rectangle(Point p1, Point p2, Point p3, Point p4) {
 }
 
 
-
-bool Rectangle::isRectangle() {
-	
+bool Rectangle::isRectangle(float length1, float length2, float width1, float width2) {
+	if (length1 == length2) {
+		if (width1 == width2) {
+			return true;
+		}
+	}
+	else {
+		return false;
+	}
 }
 
-float getLength(Point point1, Point point2) {
+bool Rectangle::isSquare(float length, float width) {
+	return false;
+}
+
+float Rectangle::getDistance(Point point1, Point point2) {
 	float p1X = point1.getXPt();
 	float p1Y = point1.getYPt();
 
 	float p2X = point2.getXPt();
 	float p2Y = point2.getYPt();
 
+	float answer;
 
+	answer = sqrt(pow((p2X - p1X), 2) + pow((p2Y - p1Y), 2));
 
-	return 0.0;
+	return abs(answer);
 }
 
 void Rectangle::setPoint1(Point pointInput) {
